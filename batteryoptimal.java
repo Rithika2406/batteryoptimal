836cf58a3be1d9cd5539ed73b62ece0fb5fafc50
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2024-12-28 10:28:39 +0530

    Add files via upload

---
+++ batteryoptimal.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner S=new Scanner(System.in);
+		int t=S.nextInt();
+		while(t>0)
+		{
+		    int x=S.nextInt();
+		    if(x>=80)
+		    {
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		    t--;
+		}
+
+	}
+}
